12b804cd65856ea22632e247ca8ebc45fd202506
VedantTalele <vedantvt2411@gmail.com>
2023-02-02 14:39:22 +0530

    Selenium Practice Programs

---
+++ CoreJavaSessions/src/day5/loops/ForLoop4.java
@@ -0,0 +1,34 @@
+package day5.loops;
+
+public class ForLoop4 {
+	public static void main(String args[]) {
+
+//		for (int i = 0; i < 3; ++i) {
+//			System.out.println("******************* Hello i: " + i);
+//		}
+
+		for (int i = 0; i < 3; ++i) {
+			System.out.println("******************* Hello i: " + i);
+			for (int j = 0; j < 2; j++) {
+				System.out.println("############### hi j: " + j);
+			}
+		}
+		
+		for (int i = 0; i < 3; ++i) {
+			System.out.println("******************* Hello i: " + i);
+			for (int j = 0; j < 2; j++) {
+				System.out.println("############### hi j: " + j);
+				for (int k = 0; k < 2; k++) {
+					System.out.println("bye k: " + k);
+				}
+			}
+		}
+		
+		System.out.println("************Infinite loop**************");
+		for (int i = 1; i >= 0; i++) {
+			if ((i % 2 == 0)) {
+				System.out.println(i);
+			}
+		}
+	}
+}
